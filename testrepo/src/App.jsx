815ca0bb4ff6ea20cbe10b52680c3a8b68ead937
tishakachhia <tishakachhia@gmail.com>
2025-11-15 14:50:11 +0500

    15/11

--- testrepo/src/App.jsx
+++ testrepo/src/App.jsx
@@ -2,33 +2,26 @@ import React from 'react'
 
 import Header from './componects/layout/header'
 import Home from './componects/pages/home'
-import {ToastContainer,toast} from 'react-toastify';
+import {ToastContainer,toast} from 'react-toastify'
 
 export default function App() {
+
+  const to = () => {
+    toast.success('success')
+  }
+
   return (
     <div>
-      <button>click me</button>
+      <button onClick={to}>click me</button>
       <Header/>
       <Home/>
       
-      
+      <ToastContainer/>
     </div>
     
   );
   
 }
-function Toast() {
-  
-  toast.error('error');
-  return(
-    <>
-    <button onClick={Toast}>Clickme</button>
-
-    <ToastContainer/>
-   </>
-  );
-}
-
 
 
 
